5ccdac96663fd130b2c52eae8e1cbd91df303f0b
rohit1narode <naroderohit100@gmail.com>
2024-08-22 12:58:11 +0530

    comment out some part of code

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -27,8 +27,3 @@
 
 // })
 
-it('passes', () => {
-  cy.visit('http://www.ajio.com/?utm_source=google&utm_medium=cpc&utm_campaign=GSB_Brand_August2019&gad_source=1&gclid=CjwKCAjwoJa2BhBPEiwA0l0ImDlDn6JbcTsEI05QWTIUaUbp1SVjsYIh6QUkXd2ceZ0RGLBDGOfNsBoClcoQAvD_BwE')
-
-  
-})
